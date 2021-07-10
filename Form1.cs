cb4d2e22905fc4f6c934af00a26097911e942e1a
iamRenadMostafa <1700553@eng.asu.edu.eg>
2021-07-10 13:46:34 +0200

    xml_open_file

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace XML_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader read = new StreamReader(File.OpenRead(open.FileName));
+                txtArea.Text = read.ReadToEnd();
+                read.Dispose();
+                txtArea.Language = FastColoredTextBoxNS.Language.XML;
+
+            }
+        }
+    }
+}
