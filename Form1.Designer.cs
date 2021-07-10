94d552e0f3fa47dd374f0d617bbadeefda5350d5
iamRenadMostafa <1700553@eng.asu.edu.eg>
2021-07-10 13:47:08 +0200

    xml_open_file

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace XML_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Open_btn = new System.Windows.Forms.Button();
+            this.txtArea = new FastColoredTextBoxNS.FastColoredTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.txtArea)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose File Location";
+            // 
+            // Open_btn
+            // 
+            this.Open_btn.Location = new System.Drawing.Point(51, 59);
+            this.Open_btn.Name = "Open_btn";
+            this.Open_btn.Size = new System.Drawing.Size(75, 33);
+            this.Open_btn.TabIndex = 2;
+            this.Open_btn.Text = "Open";
+            this.Open_btn.UseVisualStyleBackColor = true;
+            this.Open_btn.Click += new System.EventHandler(this.Open_btn_Click);
+            // 
+            // txtArea
+            // 
+            this.txtArea.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.txtArea.AutoScrollMinSize = new System.Drawing.Size(221, 18);
+            this.txtArea.BackBrush = null;
+            this.txtArea.CharHeight = 18;
+            this.txtArea.CharWidth = 10;
+            this.txtArea.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtArea.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.txtArea.Font = new System.Drawing.Font("Courier New", 9.75F);
+            this.txtArea.IsReplaceMode = false;
+            this.txtArea.Language = FastColoredTextBoxNS.Language.XML;
+            this.txtArea.Location = new System.Drawing.Point(12, 98);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.Paddings = new System.Windows.Forms.Padding(0);
+            this.txtArea.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.txtArea.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("txtArea.ServiceColors")));
+            this.txtArea.Size = new System.Drawing.Size(776, 340);
+            this.txtArea.TabIndex = 4;
+            this.txtArea.Text = "fastColoredTextBox1";
+            this.txtArea.Zoom = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.Open_btn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.txtArea)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Open_btn;
+        private FastColoredTextBoxNS.FastColoredTextBox txtArea;
+    }
+}
+
